9f3a1fad5f533ee1d2396c19e7bfae964e76d9a5
Leonid Eremin <magmadiver007@yandex.ru>
2023-06-06 23:17:14 +0300

    Исправлены замечания

--- src/test/java/praktikum/TestBun.java
+++ src/test/java/praktikum/TestBun.java
@@ -2,24 +2,47 @@ package praktikum;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.apache.commons.lang3.RandomStringUtils;
+
+
 import static org.junit.Assert.*;
 
-import static praktikum.DataForTests.*;
 
-
+@RunWith(Parameterized.class)
 public class TestBun {
     private Bun bun;
+    private String name;
+    private float price;
+
+    public TestBun(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] bunTestVariants() {
+
+        return new Object[][] {
+                {"Космобулка", 15F},
+                {"", 0.1F},
+                {null, 0F},
+                {RandomStringUtils.randomAscii(200), Float.MIN_VALUE},
+                {RandomStringUtils.randomAlphabetic(32000), Float.MAX_VALUE},
+        };
+    }
 
     @Before
     public void setUp () {
-        bun = new Bun(BUN_NAME, BUN_PRICE);
+        bun = new Bun(name, price);
     }
     @Test
     public void testGetNameReturnBunName (){
-        assertEquals(BUN_NAME, bun.getName());
+        assertEquals(name, bun.getName());
     }
     @Test
     public void testGetNameReturnBunPrice (){
-        assertEquals(BUN_PRICE, bun.getPrice(), 0f);
+        assertEquals(price, bun.getPrice(), 0f);
     }
 }
